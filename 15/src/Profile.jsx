a2418b540195d257427cbb993d1eb62449054c8d
Antimony <146525840+nofriendsatall@users.noreply.github.com>
2025-10-07 18:56:40 +0300

    Update Profile.jsx

--- 15/src/Profile.jsx
+++ 15/src/Profile.jsx
@@ -6,8 +6,20 @@ const content = 'Текст для вкладки Profile';
 
 class Profile extends React.Component {
   // BEGIN (write your solution here)
+  static contextType = ThemeContext;
 
-  // END
+  render() {
+    const { currentTheme } = this.context;
+    const content = "Текст для вкладки Profile";
+
+    return (
+      <article className={currentTheme.className}>
+        {content}
+      </article>
+    );
+  }
 }
+  // END
+
 
 export default Profile;
