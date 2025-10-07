133bdafabdf3f03e89ca9fc7c84e30842c5e9296
Antimony <146525840+nofriendsatall@users.noreply.github.com>
2025-10-07 18:58:21 +0300

    Update Item.jsx

--- 18/src/Item.jsx
+++ 18/src/Item.jsx
@@ -1,5 +1,32 @@
 import React from 'react';
 
 // BEGIN (write your solution here)
+class Item extends React.Component {
+  handleClick = () => {
+    const { task, onToggle } = this.props;
+    onToggle(task.id, task.state);
+  };
 
+  render() {
+    const { task, number } = this.props;
+    const isFinished = task.state === 'finished';
+
+    const taskContent = (
+      <a href="#" className="todo-task" onClick={this.handleClick}>
+        {task.text}
+      </a>
+    );
+
+    return (
+      <div className="row">
+        <div className="col-1">{number}</div>
+        <div className="col">
+          {isFinished ? <s>{taskContent}</s> : taskContent}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Item;
 // END
