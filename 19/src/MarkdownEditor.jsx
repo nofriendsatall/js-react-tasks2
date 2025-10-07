edfa5ebb12bd2b7b455e083ac26d516e602c81e8
Antimony <146525840+nofriendsatall@users.noreply.github.com>
2025-10-07 18:59:31 +0300

    Update MarkdownEditor.jsx

--- 19/src/MarkdownEditor.jsx
+++ 19/src/MarkdownEditor.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Editor from '@toast-ui/editor';
 
 // BEGIN (write your solution here)
-
+const MarkdownEditor = ({ onContentChange }) => {
+    const editorRef = useRef(null);
+  
+    useEffect(() => {
+      const editor = new Editor({
+        el: editorRef.current,
+        initialEditType: 'markdown',
+        previewStyle: 'vertical',
+        hideModeSwitch: true,
+      });
+  
+      editor.addHook('change', () => {
+        const content = editor.getMarkdown();
+        onContentChange(content);
+      });
+  
+      return () => {
+        editor.destroy();
+      };
+    }, [onContentChange]);
+  
+    return <div ref={editorRef} />;
+  };
+  
+  export default MarkdownEditor;
 // END
