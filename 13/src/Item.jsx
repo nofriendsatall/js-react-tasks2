bc4bb844caddea0e1c4a7c483525886796b4203c
Antimony <146525840+nofriendsatall@users.noreply.github.com>
2025-10-07 18:52:39 +0300

    Update Item.jsx

--- 13/src/Item.jsx
+++ 13/src/Item.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
 
 // BEGIN (write your solution here)
+const Item = ({ task, onRemove }) => {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-auto">
+          <button 
+            type="button" 
+            className="btn btn-primary btn-sm"
+            onClick={onRemove}
+          >
+            -
+          </button>
+        </div>
+        <div className="col">{task}</div>
+      </div>
+      <hr />
+    </div>
+  );
+};
 
+export default Item;
 // END
