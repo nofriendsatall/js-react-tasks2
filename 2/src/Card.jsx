7358292847fe9c1fdb4d13ddab98fac7381357f3
Antimony <146525840+nofriendsatall@users.noreply.github.com>
2025-10-07 18:48:03 +0300

    Update Card.jsx

--- 2/src/Card.jsx
+++ 2/src/Card.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
 // BEGIN (write your solution here)
-
+const Card = ({ title, text }) => {
+    if (!title && !text) {
+      return null;
+    }
+  
+    return (
+      <div className="card">
+        <div className="card-body">
+          {title && <h4 className="card-title">{title}</h4>}
+          {text && <p className="card-text">{text}</p>}
+        </div>
+      </div>
+    );
+  };
+  
+  export default Card;
 // END
