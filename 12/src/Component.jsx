719b081488eecd3ec01faa770d3684d5c2a5c263
Antimony <146525840+nofriendsatall@users.noreply.github.com>
2025-10-07 18:52:15 +0300

    Update Component.jsx

--- 12/src/Component.jsx
+++ 12/src/Component.jsx
@@ -1,7 +1,62 @@
 import get from 'lodash/get';
 import uniqueId from 'lodash/uniqueId';
-import React from 'react';
+import React, { useState } from 'react';
 
 // BEGIN (write your solution here)
 
+const EventLogger = () => {
+  const [log, setLog] = useState([]);
+
+  const handleAdd = (increment) => {
+    const newestValue = log.length > 0 ? log[0] : 0;
+    const newValue = newestValue + increment;
+    setLog([newValue, ...log]);
+  };
+
+  const handleIncrement = () => handleAdd(1);
+  const handleDecrement = () => handleAdd(-1);
+
+  const handleDelete = (index) => {
+    const newLog = log.filter((_, i) => i !== index);
+    setLog(newLog);
+  };
+
+  return (
+    <div>
+      <div className="btn-group font-monospace" role="group">
+        <button 
+          type="button" 
+          className="btn btn-outline-success" 
+          onClick={handleIncrement}
+        >
+          +
+        </button>
+        <button 
+          type="button" 
+          className="btn btn-outline-danger" 
+          onClick={handleDecrement}
+        >
+          -
+        </button>
+      </div>
+      
+      {log.length > 0 && (
+        <div className="list-group">
+          {log.map((value, index) => (
+            <button
+              key={index}
+              type="button"
+              className="list-group-item list-group-item-action"
+              onClick={() => handleDelete(index)}
+            >
+              {value}
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default EventLogger;
 // END
