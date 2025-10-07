45e9b3d0711ac3f33482bcf636997812a3082870
Antimony <146525840+nofriendsatall@users.noreply.github.com>
2025-10-07 18:57:00 +0300

    Update ThemeSwitcher.jsx

--- 15/src/ThemeSwitcher.jsx
+++ 15/src/ThemeSwitcher.jsx
@@ -1,12 +1,48 @@
 import React from 'react';
-import { ButtonGroup, ToggleButton } from 'react-bootstrap';
+import { ButtonGroup, ToggleButton,ToggleButtonGroup } from 'react-bootstrap';
 
 import ThemeContext from './contexts';
 
 class ThemeSwitcher extends React.Component {
   // BEGIN (write your solution here)
+  static contextType = ThemeContext;
 
-  // END
+  handleThemeChange = (selectedThemeName) => {
+    const { themes, setCurrentTheme } = this.context;
+    const theme = themes.find(t => t.name === selectedThemeName);
+    if (theme) {
+      setCurrentTheme(theme);
+    }
+  }
+
+  render() {
+    const { themes, currentTheme } = this.context;
+
+    return (
+      <ButtonGroup className="mb-3">
+        <ToggleButtonGroup
+          type="radio"
+          name="theme"
+          value={currentTheme.name}
+          onChange={this.handleThemeChange}
+        >
+          {themes.map((theme) => (
+            <ToggleButton
+              key={theme.name}
+              id={`theme-${theme.name}`}
+              value={theme.name}
+              variant="outline-primary"
+              size="sm"
+            >
+              {theme.name}
+            </ToggleButton>
+          ))}
+        </ToggleButtonGroup>
+      </ButtonGroup>
+    );
+  }
 }
+  // END
+
 
 export default ThemeSwitcher;
