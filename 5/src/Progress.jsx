9ca7101d3b878229e04402fa708d8cec55b9058c
Antimony <146525840+nofriendsatall@users.noreply.github.com>
2025-10-07 18:49:20 +0300

    Update Progress.jsx

--- 5/src/Progress.jsx
+++ 5/src/Progress.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
 // BEGIN (write your solution here)
+const Progress = ({ percentage }) => {
+  return (
+    <div className="progress">
+      <div
+        className="progress-bar"
+        role="progressbar"
+        aria-valuenow={percentage}
+        aria-valuemin="0"
+        aria-valuemax="100"
+        aria-label="progressbar"
+        style={{ width: `${percentage}%` }}
+      ></div>
+    </div>
+  );
+};
 
+export default Progress;
 // END
