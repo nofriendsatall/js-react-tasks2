ab57432cb256e956496b7ce2ecdf62b30d4fd254
Antimony <146525840+nofriendsatall@users.noreply.github.com>
2025-10-07 18:51:29 +0300

    Update Collapse.jsx

--- 10/src/Collapse.jsx
+++ 10/src/Collapse.jsx
@@ -1,6 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import cn from 'classnames';
 
 // BEGIN (write your solution here)
+const Collapse = ({ text, opened = true }) => {
+  const [isOpen, setIsOpen] = useState(opened);
 
+  const toggleCollapse = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      <p>
+        <a
+          className="btn btn-primary"
+          href="#"
+          role="button"
+          aria-expanded={isOpen}
+          onClick={toggleCollapse}
+        >
+          Link with href
+        </a>
+      </p>
+      <div className={`collapse ${isOpen ? 'show' : ''}`}>
+        <div className="card card-body">
+          {text}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Collapse;
 // END
