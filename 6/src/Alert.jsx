b216633c0717afaee82e50de5e50a443e8847ca8
Antimony <146525840+nofriendsatall@users.noreply.github.com>
2025-10-07 18:49:40 +0300

    Update Alert.jsx

--- 6/src/Alert.jsx
+++ 6/src/Alert.jsx
@@ -2,5 +2,15 @@ import cn from 'classnames';
 import React from 'react';
 
 // BEGIN (write your solution here)
+const Alert = ({ text, type }) => {
+  const alertClass = `alert alert-${type}`;
 
+  return (
+    <div className={alertClass} role="alert">
+      {text}
+    </div>
+  );
+};
+
+export default Alert;
 // END
