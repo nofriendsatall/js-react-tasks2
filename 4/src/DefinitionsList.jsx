5cb9894fbb4a3eb306b4d029085cfa2395f83769
Antimony <146525840+nofriendsatall@users.noreply.github.com>
2025-10-07 18:48:54 +0300

    Update DefinitionsList.jsx

--- 4/src/DefinitionsList.jsx
+++ 4/src/DefinitionsList.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
 // BEGIN (write your solution here)
+const DefinitionsList = ({ data }) => {
+  if (!data || data.length === 0) {
+    return null;
+  }
 
+  return (
+    <dl>
+      {data.map((item) => (
+        <React.Fragment key={item.id}>
+          <dt>{item.dt}</dt>
+          <dd>{item.dd}</dd>
+        </React.Fragment>
+      ))}
+    </dl>
+  );
+};
+
+export default DefinitionsList;
 // END
