b3c314323371c5f881d30299e675c1bf51e51319
Antimony <146525840+nofriendsatall@users.noreply.github.com>
2025-10-07 18:48:26 +0300

    Update Card.jsx

--- 3/src/Card.jsx
+++ 3/src/Card.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
 // BEGIN (write your solution here)
+const Card = ({ title = 'title', text = 'text' }) => {
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h4 className="card-title">{title}</h4>
+        <p className="card-text">{text}</p>
+      </div>
+    </div>
+  );
+};
 
+export default Card;
 // END
