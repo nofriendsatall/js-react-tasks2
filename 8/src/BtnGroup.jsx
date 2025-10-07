b1a1496d8b004b4e217fcfe64a446519476a797e
Antimony <146525840+nofriendsatall@users.noreply.github.com>
2025-10-07 18:50:39 +0300

    Update BtnGroup.jsx

--- 8/src/BtnGroup.jsx
+++ 8/src/BtnGroup.jsx
@@ -1,6 +1,33 @@
 import cn from 'classnames';
-import React from 'react';
+import React, { useState } from 'react';
 
 // BEGIN (write your solution here)
+const BtnGroup = () => {
+  const [activeButton, setActiveButton] = useState(null);
 
+  const handleClick = (buttonName) => {
+    setActiveButton(buttonName);
+  };
+
+  return (
+    <div className="btn-group" role="group" aria-label="Basic button group">
+      <button
+        type="button"
+        className={`btn btn-secondary left ${activeButton === 'left' ? 'active' : ''}`}
+        onClick={() => handleClick('left')}
+      >
+        Left
+      </button>
+      <button
+        type="button"
+        className={`btn btn-secondary right ${activeButton === 'right' ? 'active' : ''}`}
+        onClick={() => handleClick('right')}
+      >
+        Right
+      </button>
+    </div>
+  );
+};
+
+export default BtnGroup;
 // END
